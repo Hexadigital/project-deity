d838e2d503f4d28c1cc75caa9f6ba516bee32273
Frostflake <57190472+Frostflake@users.noreply.github.com>
2021-01-08 18:57:05 -0500

    Add lexicon Fixes #12

--- project-deity/database/002.sql
+++ project-deity/database/002.sql
@@ -1,2 +1,16 @@
 ALTER TABLE "project-deity".follower_classes
     ADD COLUMN tier smallint NOT NULL DEFAULT 0;
+
+ALTER TABLE "project-deity".deities
+    ADD COLUMN registered timestamp with time zone NOT NULL DEFAULT NOW();
+
+CREATE TABLE "project-deity".lexicon
+(
+    id bigserial,
+    term text NOT NULL,
+    definition text NOT NULL,
+    added timestamp with time zone NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id)
+);
+COMMENT ON TABLE "project-deity".lexicon
+    IS 'This table holds lexicon entries.';
---
+++ project-deity/lexicon.py
@@ -0,0 +1,36 @@
+# This file is part of Project Deity.
+# Copyright 2020-2021, Frostflake (L.A.)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# Returns the lexicon entry for a given term,
+# or False if not found
+async def get_definition(cursor, search_term):
+    cursor.execute('''SELECT definition
+                      FROM "project-deity".lexicon
+                      WHERE term = %s;''',
+                   (search_term.lower(), ))
+    results = cursor.fetchone()
+    if results is None:
+        return False
+    else:
+        return results["definition"]
+
+
+# Returns a list contained the  5 most recent definitions
+async def get_latest_definitions(cursor):
+    cursor.execute('''SELECT term
+                      FROM "project-deity".lexicon
+                      ORDER BY added DESC, id DESC
+                      LIMIT 5''')
+    results = cursor.fetchall()
+    return [x["term"] for x in results]
