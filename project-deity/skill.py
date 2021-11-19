02ae73164efe5aceda61023822f3aa5b718526ea
Frostflake <57190472+Frostflake@users.noreply.github.com>
2021-11-19 00:57:44 -0500

    Add placeholder for getting skill levels

---
+++ project-deity/skill.py
@@ -0,0 +1,17 @@
+# This file is part of Project Deity.
+# Copyright 2020-2021, Frostflake (L.A.)
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+
+async def get_skills_info(cursor, follower_info):
+    return {'Woodcutting': {'level': 1, 'exp': 0}, 'Mining': {'level': 1, 'exp': 0}}
